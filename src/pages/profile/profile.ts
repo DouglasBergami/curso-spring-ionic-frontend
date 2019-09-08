acd1fee23989479014f4f793a7a1e94e1e1e4ef5
douglas.bergami <douglas.t.k.d@hotmail.com>
2019-09-08 13:07:08 -0300

    Mostrando dados e imagem do cliente para página de profile

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { StorageService } from '../../services/storage.service';
+import { ClienteService } from '../../services/domain/cliente.service';
+import { ClienteDTO } from '../../models/cliente.dto';
+import { API_CONFIG } from '../../config/api.config';
 
 @IonicPage()
 @Component({
@@ -9,19 +12,34 @@ import { StorageService } from '../../services/storage.service';
 })
 export class ProfilePage {
 
-  email: string;
+  clienteDTO: ClienteDTO;
 
   constructor(public navCtrl: NavController, 
     public navParams: NavParams, 
-    public storageService: StorageService) {
+    public storageService: StorageService,
+    public clienteServive: ClienteService) {
 
   }
 
   ionViewDidLoad() {
     let localUser = this.storageService.getLocalUser();
     if (localUser && localUser.email){
-      this.email = localUser.email;
+     this.clienteServive.findByEmail(localUser.email)
+     .subscribe(response => {
+       this.clienteDTO = response;
+       this.getImageIfExists();
+      },
+      error =>{}
+      );
     }
+  }
+
+  getImageIfExists(){
+    this.clienteServive.getImageFromBucket(this.clienteDTO.id)
+    .subscribe(response => {
+      this.clienteDTO.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.clienteDTO.id}.jpg`;
+    },
+    error => {});
   }
 
 }
