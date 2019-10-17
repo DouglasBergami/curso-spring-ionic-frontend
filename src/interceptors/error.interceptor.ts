7a5097d029c5ac9a6095378c3d7d1e3e88341638
Douglas <=>
2019-10-17 00:58:40 -0300

    Salvando novo cliente

--- src/interceptors/error.interceptor.ts
+++ src/interceptors/error.interceptor.ts
@@ -1,74 +1,101 @@
-import { HttpEvent, HttpInterceptor, HttpRequest, HttpHandler,HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpEvent, HttpInterceptor, HttpRequest, HttpHandler, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable } from 'rxjs/Rx';
 import { Injectable } from '@angular/core';
 import { StorageService } from '../services/storage.service';
 import { AlertController } from 'ionic-angular';
+import { FieldMessage } from '../models/fieldmessage';
 
 @Injectable()
-export class ErrorInteceptor implements HttpInterceptor{
+export class ErrorInteceptor implements HttpInterceptor {
 
-    constructor (public storage: StorageService, public alertCrtl: AlertController){
+    constructor(public storage: StorageService, public alertCrtl: AlertController) {
 
     }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return next.handle(req)
-    .catch((error, caught) => {
+        return next.handle(req)
+            .catch((error, caught) => {
 
-        let errorObj = error;
-        if (errorObj.error){
-            errorObj = errorObj.error;
-        }
-        if (!errorObj.status){
-            errorObj = JSON.parse(errorObj);
-        }
+                let errorObj = error;
+                if (errorObj.error) {
+                    errorObj = errorObj.error;
+                }
+                if (!errorObj.status) {
+                    errorObj = JSON.parse(errorObj);
+                }
 
-        console.log(errorObj);
+                console.log(errorObj);
 
-        switch(errorObj.status){
-            case 403:
-            this.handle403();
-            break;
+                switch (errorObj.status) {
+                    case 403:
+                        this.handle403();
+                        break;
 
-            case 401:
-            this.handle401();
-            break;
+                    case 401:
+                        this.handle401();
+                        break;
 
-            default:
-            this.handleDefaultError(errorObj);
-        }
+                    case 422:
+                        this.handle422(errorObj);
+                        break;
 
-        return Observable.throw(errorObj);
-    }) as any;
+                    default:
+                        this.handleDefaultError(errorObj);
+                }
 
-}
+                return Observable.throw(errorObj);
+            }) as any;
 
-    handle403(){
+    }
+
+    handle403() {
         this.storage.setLocalUser(null);
     }
 
-    handle401(){
+    handle401() {
         let alert = this.alertCrtl.create({
             title: "Erro 401 falha de autenticação",
             message: "Email ou senha incorretos",
             enableBackdropDismiss: false,
             buttons: [
-                {text: "Ok"}
+                { text: "Ok" }
             ]
         });
         alert.present();
     }
 
-    handleDefaultError(errorObj){
+    handle422(errorObj) {
+        let alert = this.alertCrtl.create({
+            title: "Erro 422: Validação",
+            message: this.listErrors(errorObj.errors),
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: "Ok"
+                }
+            ]
+        });
+        alert.present();
+    }
+
+    handleDefaultError(errorObj) {
         let alert = this.alertCrtl.create({
             title: "Erro " + errorObj.status + " : " + errorObj.error,
             message: errorObj.message,
             enableBackdropDismiss: false,
             buttons: [
-                {text: "Ok"}
+                { text: "Ok" }
             ]
         });
         alert.present();
+    }
+
+    private listErrors(messages: FieldMessage[]): string {
+        let s: string = "";
+        for (var i = 0; i < messages.length; i++) {
+            s = s + "<p><strong>" + messages[i].fieldName + "</strong> " + messages[i].message + "</p>";
+        }
+        return s;
     }
 
 }
--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, MenuController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, MenuController, AlertController } from 'ionic-angular';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { EstadoService } from '../../services/domain/estado.service';
 import { CidadeService } from '../../services/domain/cidade.service';
 import { cidadeDTO } from '../../models/cidade.dto';
 import { estadoDTO } from '../../models/estado.dto';
+import { ClienteService } from '../../services/domain/cliente.service';
 
 @IonicPage()
 @Component({
@@ -23,12 +24,14 @@ export class SignupPage {
     public menu: MenuController,
     public formBuilder: FormBuilder,
     public estadoService: EstadoService,
-    public cidadeService: CidadeService) {
+    public cidadeService: CidadeService,
+    public clienteService: ClienteService,
+    public alertCtrl: AlertController) {
 
     this.formGroup = this.formBuilder.group({
       nome: ['Joaquim', [Validators.required, Validators.minLength(5), Validators.maxLength(120)]],
       email: ['joaquim@gmail.com', [Validators.required, Validators.email]],
-      tipo: ['1', [Validators.required]],
+      tipoCliente: ['1', [Validators.required]],
       cpfOuCnpj: ['06134596280', [Validators.required, Validators.minLength(11), Validators.maxLength(14)]],
       senha: ['123', [Validators.required]],
       logradouro: ['Rua Via', [Validators.required]],
@@ -44,29 +47,50 @@ export class SignupPage {
     })
   }
 
-  signupUser() {
-    console.log("enviou o log");
-  }
+  ionViewDidLoad() {
 
-  ionViewDidLoad(){
-
-    this.estadoService.findAll().subscribe(dados =>{
+    this.estadoService.findAll().subscribe(dados => {
       this.estadoDTO = dados;
       this.formGroup.controls.estadoId.setValue(this.estadoDTO[0].id);
       this.updateCidades();
     },
-    error =>{}
+      error => { }
     );
   }
 
-  updateCidades(){
+  updateCidades() {
     let estado_id = this.formGroup.value.estadoId;
-    this.cidadeService.findAll(estado_id).subscribe(dados =>{
+    this.cidadeService.findAll(estado_id).subscribe(dados => {
       this.cidadeDTO = dados;
       this.formGroup.controls.cidadeId.setValue(null);
     },
-    error => {}
+      error => { }
     );
   }
 
+  signupUser() {
+    this.clienteService.insert(this.formGroup.value).subscribe(response => {
+      this.showInsertOk();
+    },
+      error => { }
+    );
+  }
+
+  showInsertOk(){
+    let alert = this.alertCtrl.create({
+      title: 'Sucesso',
+      message: 'Cadastro efetuado com sucesso',
+      enableBackdropDismiss: false,
+      buttons: [
+        {
+          text: 'Ok',
+          handler: () =>{
+            this.navCtrl.pop();
+          }
+        }
+      ]
+
+    });
+    alert.present();
+  }
 }
